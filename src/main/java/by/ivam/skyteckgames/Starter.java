ae20aa1738adc4e7399c522365d47d9d42819e28
ivam_ <ivam@tut.by>
2024-01-16 12:33:40 +0300

    initial commit * Create models Clan and User * Clans lot creation class

--- src/main/java/by/ivam/skyteckgames/Starter.java
+++ src/main/java/by/ivam/skyteckgames/Starter.java
@@ -1,18 +1,12 @@
 package by.ivam.skyteckgames;
 
+import by.ivam.skyteckgames.service.ClansService;
+
 public class Starter {
 private static final int CLANS_QUANTITY = 5;
     public static void main(String[] args) {
 
-        ClansCreator.createClansLot(CLANS_QUANTITY);
+        ClansService.createClansLot(CLANS_QUANTITY);
 
-//        Random random  =  new Random();
-//        int randomNumber = random.nextInt(100);
-//        System.out.println(randomNumber);
-//        IntStream.range(0, 100).mapToObj(i -> {
-//
-//                    return null;
-//                }
-//        );
     }
 }
